90c10ee6224ed7e32ec6b20e3000aba73581867c
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-19 13:11:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sum_of_two_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+            Console.WriteLine("Enter first number");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before swapping First number="+n1+" Second Number="+n2);
+            temp = n1;
+            n1 = n2;
+            n2 = n1;
+            Console.WriteLine("After swapping First number=" + n1 + " Second Number=" + n2);
+
+        }
+    }
+}
